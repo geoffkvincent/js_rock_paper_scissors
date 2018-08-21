6d80d1f967bc5e6135be8692eca81c30b069259d
Geoff Vincent <geoffkvincent@gmail.com>
2018-08-20 22:03:46 -0600

    everything working

--- main.js
+++ main.js
@@ -1,33 +1,54 @@
-//make an array with:rock,paper,scissors
-//user choice needs to initialize random index on array
-//show result under computer
-//whatever is clicked shows under user 
+
+
+
+
 
 var rock = document.getElementById('rock')
 var paper = document.getElementById('paper')
 var scissor = document.getElementById('scissor')
 var playerChoice = document.getElementById('user')
 var computerChoice = document.getElementById('comp')
+var result = document.getElementById('result')
 var options = [ 'rock', 'paper', 'scissor' ]
 var random = ''
-// computerChoice.innerHTML = random 
-
 
 rock.addEventListener('click', function() {
   playerChoice.innerHTML = 'rock'
   random = options[Math.floor(Math.random() * options.length)]
   computerChoice.innerHTML = random 
+   if (random == 'rock') {
+     result.innerHTML = 'TIE'
+   } else if (random == 'paper') {
+     result.innerHTML = 'LOSER'
+   } else if (random == 'scissor') {
+     result.innerHTML = 'WINNER!'
+   }
+
 })
 
 paper.addEventListener('click', function() {
   playerChoice.innerHTML = 'paper'
   random = options[Math.floor(Math.random() * options.length)]
   computerChoice.innerHTML = random 
+  if (random == 'paper') {
+    result.innerHTML = 'TIE'
+  } else if (random == 'scissor') {
+    result.innerHTML = 'LOSER'
+  } else if (random == 'rock') {
+    result.innerHTML = 'WINNER!'
+  }
+  
 })
 
 scissor.addEventListener('click', function() {
   playerChoice.innerHTML = 'scissors'
   random = options[Math.floor(Math.random() * options.length)]
   computerChoice.innerHTML = random 
+  if (random == 'scissor') {
+    result.innerHTML = 'TIE'
+  } else if (random == 'rock') {
+    result.innerHTML = 'LOSER'
+  } else if (random == 'paper') {
+    result.innerHTML = 'WINNER!'
+  }
 })
-
